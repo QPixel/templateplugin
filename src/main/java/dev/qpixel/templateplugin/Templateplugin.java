c33d825a09dcf3f575a1d1bce1b3305f57b9f1ce
Riley <riley@rileysmith.me>
2021-07-24 12:48:18 -0700

    First Commit

---
+++ src/main/java/dev/qpixel/templateplugin/Templateplugin.java
@@ -0,0 +1,17 @@
+package dev.qpixel.templateplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Templateplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
